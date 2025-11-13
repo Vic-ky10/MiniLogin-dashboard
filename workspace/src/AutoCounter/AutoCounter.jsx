1ccf30584fea665494c1829639d67ab40d18a7a7
vigneshwaran <vigneshvenketasan@gmail.com>
2025-11-13 23:09:24 +0530

    creating autoCounter

--- workspace/src/AutoCounter/AutoCounter.jsx
+++ workspace/src/AutoCounter/AutoCounter.jsx
@@ -1,48 +1,57 @@
-
-
-import React, {useRef, useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState } from "react";
 
 function AutoCounter() {
+  const [count, setCount] = useState(0);
+  const [running, setRunning] = useState(false);
+  const intervelRef = useRef(null);
 
-    const[count ,setCount] = useState(0)
-    const [running ,setRunning] = useState(false) ;
-    const intervelRef = useRef(null);
-
-    useEffect (() => {
-        if(running){
-  intervelRef.current = setInterval(() => {
-            setCount (p => p +1 )
-        },1000)
-        }
-      
-
-        return ()=> clearInterval ( intervelRef.current)
-    },[running])
-
-    const start = () => setRunning(true)
-    const stop = () => {
-        setRunning(false);
-        clearInterval(intervelRef.current);
-
+  useEffect(() => {
+    if (running) {
+      intervelRef.current = setInterval(() => {
+        setCount((p) => p + 1);
+      }, 1000);
     }
-      const reset = () => {
-        setRunning(false);
-        clearInterval(intervelRef.current);
-        setCount(0)
-        
-    }
+
+    return () => clearInterval(intervelRef.current);
+  }, [running]);
+
+  const start = () => setRunning(true);
+  const stop = () => {
+    setRunning(false);
+    clearInterval(intervelRef.current);
+  };
+  const reset = () => {
+    setRunning(false);
+    clearInterval(intervelRef.current);
+    setCount(0);
+  };
   return (
-    <div style={{textAlign: "center" ,marginTop :" 50px"}}>
-        <h2>AutoCounter</h2>
-<button onClick={start}>Start</button>
-<button onClick={stop}> stop</button>
-<button onClick={reset}> reset</button>
-
-
-
-
+    <div style={{ textAlign: "center", marginTop: " 50px" }}>
+      <h1>Smart Counter</h1>
+      <h2 style={{ fontSize: "60px" }}>{count}</h2>
+      <button onClick={start} style={btnStyle} disabled={running}>
+        Start
+      </button>
+      <button onClick={stop} style={btnStyle} disabled={!running}>
+        {" "}
+        stop
+      </button>
+      <button onClick={reset} style={btnStyle}>
+        {" "}
+        reset
+      </button>
     </div>
-  )
+  );
 }
 
-export default AutoCounter
+const btnStyle = {
+  padding: "10px 20px",
+  margin: "20px",
+  borderRadius: " 25%",
+ 
+  cursor: "pointer",
+
+  
+};
+
+export default AutoCounter;
